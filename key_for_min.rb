4399f36d6eb91126971e8a67bc3f9966ee85af4c
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-21 04:30:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,9 @@ def key_for_min_value(name_hash)
   smallest_value=nil 
   smallest_key=nil
   name_hash.each do |key,value|
-    if 
+    if smallest_value==nil || value<smallest_value
+      smallest_value=value
+      smallest_key=key
+    end
+    smallest_key
 end
